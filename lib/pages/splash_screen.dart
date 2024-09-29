5ed989a444257eed79ad2a1e0e4218d7a493f489
vicente-santander <127953756+vicente-santander@users.noreply.github.com>
2024-09-29 16:46:54 -0300

    screen splash desvanecer

--- lib/pages/splash_screen.dart
+++ lib/pages/splash_screen.dart
@@ -9,15 +9,27 @@ class SplashScreen extends StatefulWidget {
 }
 
 class SplashScreenState extends State<SplashScreen> {
+  double _opacity = 1.0; // Inicialmente opacidad completa
+
   @override
   void initState() {
     super.initState();
+    _startFadeOut();
     _navigateToHomePage();
   }
 
+  Future<void> _startFadeOut() async {
+    // Espera unos segundos antes de iniciar el desvanecimiento
+    await Future.delayed(const Duration(seconds: 2));
+    setState(() {
+      // Cambia la opacidad a 0.0 para hacer que la imagen se desvanezca
+      _opacity = 0.0;
+    });
+  }
+
   Future<void> _navigateToHomePage() async {
-    // Simula un tiempo de carga
-    await Future.delayed(const Duration(seconds: 4));
+    // Simula un tiempo de carga total (incluyendo el tiempo de desvanecimiento)
+    await Future.delayed(const Duration(seconds: 3));
 
     // Verifica si el widget aún está montado antes de usar el BuildContext
     if (mounted) {
@@ -34,10 +46,15 @@ class SplashScreenState extends State<SplashScreen> {
     return Scaffold(
       backgroundColor: Colors.white, // Fondo de la pantalla de presentación
       body: Center(
-        child: Image.asset(
-          'assets/icons/espresso_dreams.png',
-          width: 250, // Ajusta el tamaño del logo
-          height: 250,
+        child: AnimatedOpacity(
+          opacity: _opacity, // Opacidad controlada por el estado
+          duration: const Duration(
+              seconds: 1), // Duración de la animación de desvanecimiento
+          child: Image.asset(
+            'assets/icons/espresso_dreams.png',
+            width: 250, // Ajusta el tamaño del logo
+            height: 250,
+          ),
         ),
       ),
     );
